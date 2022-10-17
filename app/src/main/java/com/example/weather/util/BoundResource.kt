a8ca9c8f23d8bc54c8ff80460be928119b27f729
User 1 <amirnse017@gmail.com>
2022-10-17 17:52:32 +0330

    cache mechanism improved. (version 1.3)

--- app/src/main/java/com/example/weather/util/BoundResource.kt
+++ app/src/main/java/com/example/weather/util/BoundResource.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 val TIME_STEP_REQUEST = TimeUnit.MINUTES.toMillis(5)
 
-suspend fun <I, O1, O2, O3> boundResource(
+fun <I, O1, O2, O3> boundResource(
     query1: () -> Flow<O1>,
     query2: () -> Flow<O2>,
     query3: () -> Flow<O3>,
@@ -18,7 +18,7 @@ suspend fun <I, O1, O2, O3> boundResource(
     saveFetchResult: suspend (I) -> Unit,
     shouldFetch: suspend () -> Boolean = { true }
 
-): Triple<MutableStateFlow<ResultOf<O1>>, MutableStateFlow<ResultOf<O2>>, MutableStateFlow<ResultOf<O3>>> {
+): Triple<MutableStateFlow<ResultOf<O1>>, MutableStateFlow<ResultOf<O2>>, MutableStateFlow<ResultOf<O3>>> = runBlocking {
 
     val stateFlow1: MutableStateFlow<ResultOf<O1>> =
         MutableStateFlow(ResultOf.Loading)
@@ -27,28 +27,30 @@ suspend fun <I, O1, O2, O3> boundResource(
     val stateFlow3: MutableStateFlow<ResultOf<O3>> =
         MutableStateFlow(ResultOf.Loading)
 
-    if (MainApp.isConnected()) {
-        try {
-            val data = fetch()
-            saveFetchResult(data)
-
+    try {
+        val data:I = fetch()
+        saveFetchResult(data)
+        async {
             query1().collect {
                 stateFlow1.emit(ResultOf.Success(it))
             }
-
+        }
+        async {
             query2().collect {
                 stateFlow2.emit(ResultOf.Success(it))
             }
+        }
+        async {
             query3().collect {
                 stateFlow3.emit(ResultOf.Success(it))
             }
-        } catch (e: Exception) {
-            stateFlow1.emit(ResultOf.Error(e))
-            stateFlow2.emit(ResultOf.Error(e))
-            stateFlow3.emit(ResultOf.Error(e))
         }
+    } catch (e: Exception) {
+        stateFlow1.emit(ResultOf.Error(e))
+        stateFlow2.emit(ResultOf.Error(e))
+        stateFlow3.emit(ResultOf.Error(e))
     }
 
 
-    return Triple(stateFlow1, stateFlow2, stateFlow3)
+    Triple(stateFlow1, stateFlow2, stateFlow3)
 }
