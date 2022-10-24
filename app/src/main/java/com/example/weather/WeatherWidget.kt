35bccd9b06d7f6f815b86e129ef896d059be1b6f
User 1 <amirnse017@gmail.com>
2022-10-24 16:22:57 +0330

    app widget has been completed.(version 1.1)

--- app/src/main/java/com/example/weather/WeatherWidget.kt
+++ app/src/main/java/com/example/weather/WeatherWidget.kt
@@ -1,63 +1,72 @@
 package com.example.weather
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
 import com.example.weather.data.domain.WeatherRepository
+import com.example.weather.util.WidgetContent
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-//@AndroidEntryPoint
-class WeatherWidget /*@Inject constructor(
-    private val repository: WeatherRepository
-)*/ : AppWidgetProvider() {
-    //private lateinit var sharedPreferences: SharedPreferences
-    override fun onUpdate(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray
-    ) {
-        /*sharedPreferences = context.getSharedPreferences(
-            context.getString(R.string.coordinates),
-            Context.MODE_PRIVATE
-        )
-        val coordinate: String =
-            sharedPreferences.getString(context.getString(R.string.coordinates), "Tehran")
-                ?: "Toronto"
-        val map = mapOf(
-            "key" to ApiConfigurations.API_KEY,
-            "days" to "7",
-            "aqi" to "no",
-            "alerts" to "no",
-            "q" to coordinate
-        )
-        runBlocking {
-            repository.getData(map).collect{
-                when(it){
-                    is ResultOf.Success -> {
+@AndroidEntryPoint
+class WeatherWidget : AppWidgetProvider() {
 
-                    }
-                    else -> {}
+    @Inject
+    lateinit var repository: WeatherRepository
+    private val job = SupervisorJob()
+    private val coroutineScope = CoroutineScope(Dispatchers.IO + job)
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+
+        coroutineScope.launch {
+            val data = repository.getDataForWidget()
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            val man = AppWidgetManager.getInstance(context)
+            val ids =
+                man.getAppWidgetIds(context?.let { ComponentName(it, WeatherWidget::class.java) })
+
+            if (data != null && context != null) {
+                for (appWidgetId in ids) {
+                    updateAppWidget(
+                        context, appWidgetManager, appWidgetId, data
+                    )
                 }
-            }
-        }*/
 
-        Log.d("WIDGET", "WIDGET")
-        for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            }
         }
     }
 }
 
-
 internal fun updateAppWidget(
     context: Context,
     appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
+    appWidgetId: Int,
+    content: WidgetContent
 ) {
     val views = RemoteViews(context.packageName, R.layout.weather_widget)
+    views.setTextViewText(R.id.temp_widget_tv, content.temperature.toString())
+    views.setTextViewText(R.id.date_widget_tv, content.date)
+    views.setTextViewText(R.id.territory_name_tv, content.territoryName)
+    //TODO("update icon")
+
+    views.setOnClickPendingIntent(R.id.widget_root,
+        getPendingIntentActivity(context))
 
     appWidgetManager.updateAppWidget(appWidgetId, views)
+}
+
+private fun getPendingIntentActivity(context: Context): PendingIntent
+{
+    val intent = Intent(context, MainActivity::class.java)
+    return PendingIntent.getActivity(context, 0, intent, 0)
 }
