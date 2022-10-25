7923c4352c5a1e6dbd1d9123519d7c2741057274
User 1 <amirnse017@gmail.com>
2022-10-25 12:54:07 +0330

    landscape views added.

--- app/src/main/java/com/example/weather/util/Mapper.kt
+++ app/src/main/java/com/example/weather/util/Mapper.kt
@@ -103,30 +103,30 @@ object Mapper {
             )
             val dayOfWeek: String = calendar.get(Calendar.DAY_OF_WEEK).let { it1 ->
                 when (it1) {
-                    1 -> "Sun"
-                    2 -> "Mon"
-                    3 -> "Tue"
-                    4 -> "Wed"
-                    5 -> "Thu"
-                    6 -> "Fri"
-                    7 -> "Sat"
+                    4 -> "Sun"
+                    5 -> "Mon"
+                    6 -> "Tue"
+                    7 -> "Wed"
+                    1 -> "Thu"
+                    2 -> "Fri"
+                    3 -> "Sat"
                     else -> "None"
                 }
             }
             val month: String = calendar.get(Calendar.MONTH).let { it2 ->
                 when (it2) {
-                    0 -> "Jan"
-                    1 -> "Feb"
-                    2 -> "Mar"
-                    3 -> "Apr"
-                    4 -> "May"
-                    5 -> "Jun"
-                    6 -> "Jul"
-                    7 -> "Aug"
-                    8 -> "Sep"
-                    9 -> "Oct"
-                    10 -> "Nov"
-                    11 -> "Dec"
+                    1 -> "Jan"
+                    2 -> "Feb"
+                    3 -> "Mar"
+                    4 -> "Apr"
+                    5 -> "May"
+                    6 -> "Jun"
+                    7 -> "Jul"
+                    8 -> "Aug"
+                    9 -> "Sep"
+                    10 -> "Oct"
+                    11 -> "Nov"
+                    12 -> "Dec"
                     else -> "None"
                 }
             }
